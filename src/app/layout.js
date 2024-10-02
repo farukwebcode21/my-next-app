1e4c28b4d607aea6e95692e109893539893af092
farukwebcode21 <farukahmadmk@yahoo.com>
2024-10-02 22:40:42 +0600

    start nextjs learning

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="flex items-center justify-center px-10 py-10 m-0 text-2xl ">
         {children}
       </body>
     </html>
