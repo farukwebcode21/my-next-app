2c31e98ea28fb4cefc44249386cc08d34e3b42f8
farukwebcode21 <farukahmadmk@yahoo.com>
2024-10-15 22:36:02 +0600

    add extra font

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import React from "react";
+import { Headland_One } from "next/font/google";
+
+const headland = Headland_One({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata = {
   title: "my-next-app|About",
@@ -8,7 +11,7 @@ export const metadata = {
 
 const AboutPage = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className={`${headland.className}hero bg-base-200 min-h-screen`}>
       <div className="hero-content flex-col lg:flex-row-reverse">
         <Image
           src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
@@ -19,7 +22,7 @@ const AboutPage = () => {
         />
         <div>
           <h1 className="text-5xl font-bold">About Page!</h1>
-          <h1 className="text-2xl font-bold">Box Office News!</h1>
+          <h1 className="text-xl font-bold">Box Office News!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import React from "react";
+import { Headland_One } from "next/font/google";
+
+const headland = Headland_One({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata = {
   title: "my-next-app|post",
@@ -11,15 +14,15 @@ const PostsPage = async () => {
   const posts = await data.json();
 
   return (
-    <div>
+    <div className={`${headland.className}`}>
       <ul className="grid grid-cols-3 gap-5">
         {posts.slice(0, 10).map((post) => (
           <div
             className="flex flex-col border mb-4 p-4 items-center justify-center "
             key={post.id}
           >
-            <li className="text-2xl">Title :{post.title}</li>
-            <p className="text-xl">{post.body}</p>
+            <li className="text-sm">Title :{post.title}</li>
+            <p className="text-xs">{post.body}</p>
             <Link
               href={`/posts/${post.id}`}
               className="btn mt-5 bg-primary text-black"
